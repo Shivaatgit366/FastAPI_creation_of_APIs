52c147163d8ca5cb44f2b8988855ed3ff790102a
shiva <shiva.nanda366@gmail.com>
2021-10-11 04:09:09 +0530

    fast api learn

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from typing import Optional
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI()  # object is created by calling the FastAPI class.
 
 
+@app.get("/")  # "/" means base url, original server url. Path or the endpoint is local host itself.
+def index():
+    return {"data": {"name": "shiva", "age": 26}}
+
+
+@app.get("/about")  # get function takes the endpoint/path as an argument.
+def about():
+    return {"data": {"about page": "shiva is a good boy"}}
